c926de47757612577ae55c052b59e4ed5a3f3614
Raúl Blanco <rbr007.movil@gmail.com>
2018-03-27 23:57:07 +0200

    Cleanup and more functionality
    - Deleted root files from old version
    - Cleaned CSS file a little
    - Added function to "Day/Night" button
    - Added animation to video title (for large titles)
    - Added alert message for invalid URL
    - Added homepage URL to package.json
    - Changed test video to a non-copyrighted work

--- public/code.js
+++ public/code.js
@@ -1,12 +1,22 @@
 function getYtID(url){
   var ytID = /(?:(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\/?\?v=(.+))|(?:(?:https?:\/\/)?(?:www\.)?youtu\.be\/(.+))/.exec(url);
-  return ytID[1] || ytID[2];
+  if (ytID !== null)
+    return ytID[1] || ytID[2];
+  else
+    invalidURL();
 }
+
+// Invalid URL message
+function invalidURL(){
+  $("div#alert").text("Invalid URL");
+  $("div#alert").slideToggle();
+};
 
 function showFormats(response){
   //TODO: Show format buttons on page to be able to select quality
 }
 
+// Get Audio URL (best audio)
 function getAudioURL(ytID, id){
   var queryAudio = null;
   if (ytID !== undefined){
@@ -36,14 +46,30 @@ function preparePlayer(response){
 }
 
 $(document).ready(function(){
+  // Test button
   $("input#test").click(function(){
-    $("input#videoURL").val("https://youtu.be/NWks-8C9RMs?t=2m30s");
+    $("input#videoURL").val("https://www.youtube.com/watch?v=bM7SZ5SBzyY");
     $("input#videoURL").change();
   });
+
+  // Switch color button
+  $("button#changeSkinButton").click(function(){
+    $("div#AppContainer").toggleClass("AppDark AppLight");
+    $("header#AppHeader").toggleClass("App-headerDark App-headerLight");
+    $(this).text($(this).text() == "Day mode" ? "Night mode" : "Day mode");
+  });
+
+  // URL Input AJAX
   $("input#videoURL").change(function(){
+    // Clear alerts
+    $("div#alert").slideUp();
+
+    // Get valid YouTube ID
     if ($(this).val().trim() != "")
       var ytID = getYtID($(this).val());
-    if (ytID !== null){
+
+    // AJAX
+    if (ytID !== null || ytID !== undefined){
       $.ajax({
         async: true,
         type: "GET",
