6dcb1394a2b79b559ea8260a95441a57a17fa2ce
Alkesst <alec.augal@gmail.com>
2018-03-29 17:08:36 +0200

    Deleted code.js and integrated into the React app. Done minor changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,87 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+      super(props);
+      this.state = ({
+          youtubeVideoURL: "",
+          qualityFromAudio: 0,
+          invalidURL: false,
+          youtubeAudioURL: "",
+          youtubeVideoTitle: "",
+          loading: false,
+          error: false,
+          nightMode: true
+      });
+      this.listenerForm = this.listenerForm.bind(this);
+      this.listenerTestButton = this.listenerTestButton.bind(this);
+      this.nightModeListener = this.nightModeListener.bind(this);
+  }
+
+    _testYoutubeVideoURL = "https://www.youtube.com/watch?v=bM7SZ5SBzyY";
+
+  listenerTestButton(event){
+    event.preventDefault();
+    this.setState({youtubeVideoURL: this._testYoutubeVideoURL});
+    this.loadAudioURL(this.getYoutubeVideoID(this._testYoutubeVideoURL));
+  }
+
+  listenerForm(event) {
+      let url = event.target.value;
+      let newState = {youtubeVideoURL: url};
+      let youtubeVideoID = this.getYoutubeVideoID(url);
+      if (youtubeVideoID !== null){
+          newState.invalidURL = false;
+          this.loadAudioURL(youtubeVideoID);
+      } else {
+          newState.invalidURL = url.length !== 0;
+      }
+      this.setState(newState);
+  }
+
+  loadAudioURL(youtubeVideoID) {
+      setTimeout(() => this.setState({ loading: true, error: false }));
+      $.ajax({
+          async: true,
+          type: "GET",
+          url: "https://yt-audio-api.herokuapp.com/api/" + youtubeVideoID,
+          success: (response) => {
+              this.setState({youtubeAudioURL: response.url, youtubeVideoTitle: response.title, loading: false});
+              console.log(response);
+          },
+          error: () => this.setState({ loading: false, youtubeAudioURL: "", youtubeVideoTitle: "", error: true })
+      });
+  }
+
+  getYoutubeVideoID(url){
+    let youtubeVideoID =
+        /(?:(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\/?\?v=(.+))|(?:(?:https?:\/\/)?(?:www\.)?youtu\.be\/(.+))/
+            .exec(url);
+    if (youtubeVideoID !== null)
+        return youtubeVideoID[1] || youtubeVideoID[2];
+    else
+        this.setState({loading: false});
+        return null;
+  }
+
+  nightModeListener(event){
+      event.preventDefault();
+      this.setState({ nightMode: !this.state.nightMode });
+  }
+
+
   render() {
+      const { youtubeVideoURL, invalidURL, youtubeVideoTitle, youtubeAudioURL, loading, error, nightMode } = this.state;
     return (
-      <div className="AppDark fill" id="AppContainer">
-        <header className="App-headerDark" id="AppHeader">
+      <div className={`${nightMode ? 'AppDark' : 'AppLight'} fill`} id="AppContainer">
+        <header className={`${nightMode ? 'App-headerDark' : 'App-headerLight'}`} id="AppHeader">
             <h1 className="App-title">YouTube Audio Player</h1>
-            <button className="btn btn-outline-light" id="changeSkinButton">Day mode</button>
+            <button className="btn btn-outline-light" id="changeSkinButton" onClick={ this.nightModeListener }>
+                { !nightMode ? "Night Mode" : "Day mode"}
+            </button>
         </header>
           <div className="container-fluid">
               <h5 id="title">Enter Youtube video Link</h5>
@@ -17,18 +90,38 @@ class App extends Component {
                   <div className="col-md-6 col-sm-12">
                       <div className="input-group" id="input">
                           <div className="input-group-prepend">
-                              <input type="button" className="input-group-text btn btn-outline-dark" id="test" name="test" value="TEST"/>
+                              <input type="button" className="input-group-text btn btn-outline-dark"
+                                     id="test" name="test" value="TEST" onClick={ this.listenerTestButton }
+                                     disabled={loading}/>
                           </div>
-                          <input type="text" className="form-control" id="videoURL" name="videoURL"/>
+                          <input type="text" className={`form-control ${invalidURL ? 'is-invalid' : ''}`}
+                                 id="videoURL" name="videoURL" onChange={ this.listenerForm }
+                                 value={ youtubeVideoURL } placeholder="insert here your youtube video url..."
+                                 disabled={loading}/>
                       </div>
                       <div className="alert alert-danger" id="alert" role="alert">
-                          
                       </div>
-                      <div className="title" id="NP">
-                          Now Playing:
-                          <div id="title" className="text-center"/>
-                      </div>
-                      <audio id="player" className="player" controls/>
+                      {loading ?
+                          <div className="title" id="NP">
+                                Loading...
+                          </div> : null
+                      }
+                      { error ?
+                          <div className="title" id="NP">
+                              Video not found...
+                          </div> : null
+                      }
+                      { this.state.youtubeVideoTitle ?
+                          <div className="title" id="NP">
+                              <div id="NowPlaying">
+                                  Now Playing:
+                              </div>
+                              <div id="title" className="text-center"> { youtubeVideoTitle } </div>
+                          </div> : null
+                      }
+                      { this.state.youtubeAudioURL ?
+                        <audio id="player" className="player" controls src={ youtubeAudioURL } /> : null
+                      }
                   </div>
               </div>
           </div>
