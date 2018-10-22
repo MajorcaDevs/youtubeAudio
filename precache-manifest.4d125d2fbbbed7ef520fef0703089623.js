1d7b8a978346edac512bf5979dbac8a7fe6eab54
melchor629 <melchor9000@gmail.com>
2018-10-22 23:54:38 +0200

    Updates

---
+++ precache-manifest.4d125d2fbbbed7ef520fef0703089623.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "960ee6c38eccf70dba196e72ee97353f",
+    "url": "/youtubeAudio/static/media/github.960ee6c3.svg"
+  },
+  {
+    "revision": "5e26b7cf26472b59f08f",
+    "url": "/youtubeAudio/static/css/main.700f2006.chunk.css"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/youtubeAudio/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "ae5a234aa9363bd57fcb",
+    "url": "/youtubeAudio/static/js/1.ae5a234a.chunk.js"
+  },
+  {
+    "revision": "649f91976233d59e8daf",
+    "url": "/youtubeAudio/static/js/runtime~main.649f9197.js"
+  },
+  {
+    "revision": "5e26b7cf26472b59f08f",
+    "url": "/youtubeAudio/static/js/main.5e26b7cf.chunk.js"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/youtubeAudio/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/youtubeAudio/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/youtubeAudio/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "ae5a234aa9363bd57fcb",
+    "url": "/youtubeAudio/static/css/1.142b35fe.chunk.css"
+  },
+  {
+    "revision": "b9d872946cee47e2acb6567c28c9dc5f",
+    "url": "/youtubeAudio/index.html"
+  }
+];
